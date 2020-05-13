35740bac6ba981b47a3a6fdcd96c5005ad2bc650
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-13 19:30:54 +0530

    Updated coupons spider

--- Udemy_Coupons/Udemy_Coupons/spiders/coupons.py
+++ Udemy_Coupons/Udemy_Coupons/spiders/coupons.py
@@ -8,7 +8,7 @@ class Coupons(Spider):
 	name = 'coupons'
 	start_urls = [
 					'https://smartybro.com/category/udemy-coupon-100-off/',
-					# 'https://comidoc.net/coupons',
+					'https://comidoc.net/coupons',
 					# 'https://udemycoupon.learnviral.com/',
 					# 'https://www.real.discount/store/udemy/'
 					]
@@ -31,13 +31,19 @@ class Coupons(Spider):
 			url = response.urljoin(href.extract())
 			yield Request(url, callback = self.parse_smartybro_each_page)
 
+		# not running pagination because the old posts contains expired coupons
+		# next_page = response.css('a.next::attr(href)').get()
+		# if next_page is not None:
+		# 	yield response.follow(next_page,callback = self.parse_smartybro)
+
 	def parse_smartybro_each_page(self,response):
 		items = UdemyCouponsItem()
+		items['site'] = 'SmartyBro'
 		items['link'] = response.css('a.fasc-type-flat::attr(href)').extract_first()
 		items['name'] = response.css('span.entry-title::text').extract_first()
 		items['tags'] = response.xpath('//a[@rel="tag"]/text()').extract()
 		parameters = parse_qs(urlparse.urlparse(items['link']).query)
-		items['code'] = parameters['couponCode'] if 'couponCode' in parameters else parameters['deal_code']
+		items['code'] = parameters['couponCode'][0] if 'couponCode' in parameters else "N/A"
 
 		return items
 
