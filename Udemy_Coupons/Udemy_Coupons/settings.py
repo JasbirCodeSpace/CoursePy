fde2b1660331846738765e11f5cb96f9d9a60e90
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-05-13 19:47:36 +0530

    Other changes

--- Udemy_Coupons/Udemy_Coupons/settings.py
+++ Udemy_Coupons/Udemy_Coupons/settings.py
@@ -64,9 +64,9 @@ CONCURRENT_REQUESTS = 32
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'Udemy_Coupons.pipelines.UdemyCouponsPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'Udemy_Coupons.pipelines.UdemyCouponsPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
