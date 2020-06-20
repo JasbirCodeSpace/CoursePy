08080f95e999dabcf27567d869939943fd6ebcf6
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-06-20 14:04:35 +0530

    Updated udemy coupon spider

--- Udemy_Coupons/Udemy_Coupons/spiders/coupons.py
+++ Udemy_Coupons/Udemy_Coupons/spiders/coupons.py
@@ -9,8 +9,8 @@ class Coupons(Spider):
 	start_urls = [
 					'https://smartybro.com/category/udemy-coupon-100-off/',
 					'https://comidoc.net/coupons',
-					# 'https://udemycoupon.learnviral.com/',
-					# 'https://www.real.discount/store/udemy/'
+					'https://udemycoupon.learnviral.com/',
+					'https://www.real.discount/store/udemy/'
 					]
 	def start_requests(self):
 		for url in self.start_urls:
@@ -18,10 +18,10 @@ class Coupons(Spider):
 				yield Request(url , callback=self.parse_smartybro)
 			elif url == 'https://comidoc.net/coupons':
 				yield Request(url , callback=self.parse_comidoc)
-			# elif url == 'https://udemycoupon.learnviral.com/':
-			# 	yield Request(url , callback=self.parse_learnviral)
-			# else :
-			# 	yield Request(url , callback=self.parse_realdiscount)
+			elif url == 'https://udemycoupon.learnviral.com/':
+				yield Request(url , callback=self.parse_learnviral)
+			else :
+				yield Request(url , callback=self.parse_realdiscount)
 
 	def parse_smartybro(self,response):
 		all_items = response.css('div.item')
@@ -56,18 +56,43 @@ class Coupons(Spider):
 	def parse_comidoc_each_page(self,response):
 		items = UdemyCouponsItem()
 		items['site'] = 'Comidoc'
-		items['name'] = response.css('h2.selectorgadget_selected').extract_first()
+		items['name'] = response.xpath('//*[@id="__next"]/div/div/div/div[1]/h1/text()').extract_first()
 		items['tags'] = response.css('div.hvbkPj+ div p::text').extract()
 		temp  = response.xpath('//script[@id="__NEXT_DATA__"]/text()').extract_first().split(',')
 		items['code'] = [s for s in temp if "code" in s][0].split(':')[1].strip('"')
-		items['link'] = "https://www.udemy.com/course/"+[s for s in temp if "cleanUrl" in s][0].split(':')[-1].strip('"').strip('}')+"?couponCode="+items['code']
-		print("comidoc",items)
+		items['link'] = 'https://www.udemy.com/course/'+[s for s in temp if "cleanUrl" in s][0].split(':')[-1].strip('"').strip('}').strip('"')+'?couponCode='+items['code']
+		# print("comidoc",items)
 		return items
 
 
 
-	# def parse_learnviral(self,response):
-	# 	print(3)
-	# def parse_realdiscount(self,response):
-	# 	print(4)
+	def parse_learnviral(self,response):
+		all_items = response.css('.entry-title a::attr(href)').extract()
+		for url in all_items:
+			yield Request(url,callback=self.parse_learnviral_each_page)
+
+	def parse_learnviral_each_page(self,response):
+		items = UdemyCouponsItem()
+		items['site'] = 'Learnviral'
+		items['name'] = response.css('.entry-title a::text').extract_first()
+		items['link'] = response.css('a.promotion::attr(href)').extract_first()
+		items['tags'] = response.css('p.meta a::text').extract()
+		parameters = parse_qs(urlparse.urlparse(items['link']).query)
+		items['code'] = parameters['couponCode'][0] if 'couponCode' in parameters else 'N/A'
+		return items
+		
+	def parse_realdiscount(self,response):
+		all_items = response.css('.embed-responsive a::attr(href)').extract()
+		for url in all_items:
+			yield Request(url,callback=self.parse_realdiscount_each_page)
+
+	def parse_realdiscount_each_page(self,response):
+		items = UdemyCouponsItem()
+		items['site'] = 'RealDiscount'
+		items['name'] = response.css('.breadcrumb_last::text').extract_first()
+		items['link'] = response.css('div.deal-sidebar-box div.letshover a::attr(href)').extract_first()
+		items['tags'] = response.css('.tags-list1 div a::text').extract()
+		parameters = parse_qs(urlparse.urlparse(items['link']).query)
+		items['code'] = parameters['couponCode'][0] if 'couponCode' in parameters else 'N/A'
+		return items
 
