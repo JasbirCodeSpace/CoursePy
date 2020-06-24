1e144056a0d33bd28d8bfbbad8c69ad66db2301b
JasbirCodeSpace <shikhawat.jasbir@gmail.com>
2020-06-24 14:12:16 +0530

    Added database operations in pipeline

--- Coursera/Coursera/pipelines.py
+++ Coursera/Coursera/pipelines.py
@@ -5,7 +5,45 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import sqlite3
 
 class CourseraPipeline:
-    def process_item(self, item, spider):
-        return item
+	db_file = 'coursera.db'
+	db_table = 'coursera' 
+	def __init__(self):
+		self.create_connection()
+		self.create_table()
+
+	def create_connection(self):
+		self.conn = sqlite3.connect(self.db_file)
+		self.curr = self.conn.cursor()
+
+	def create_table(self):
+		self.curr.execute(""" DROP TABLE IF EXISTS %s """ % self.db_table)
+		self.curr.execute(""" CREATE TABLE %s (
+			name TEXT,
+			university TEXT,
+			category TEXT,
+			difficulty TEXT,
+			rating TEXT,
+			enrollment TEXT,
+			image TEXT,
+			link TEXT)""" % self.db_table)
+		self.conn.commit()
+
+
+	def insert_row(self,item):
+		self.curr.execute(""" INSERT INTO %s VALUES(?,?,?,?,?,?,?,?)""" % self.db_table,(
+			item['name'],
+			item['university'],
+			item['category'],
+			item['difficulty'],
+			item['rating'],
+			item['enrollment'],
+			item['image'],
+			item['link']))
+		self.conn.commit()
+
+	def process_item(self, item, spider):
+		self.insert_row(item)
+		return item
